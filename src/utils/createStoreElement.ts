1ed0cf2f128243e53d116d41c41761b6524ca78b
Shen Chang <s2696922797c@outlook.com>
2019-04-29 20:55:31 +0800

    fix(createStoreElement.ts): Change `display: none` to `visibility: hidden; opacity: 0; position: abs

--- src/utils/createStoreElement.ts
+++ src/utils/createStoreElement.ts
@@ -3,7 +3,12 @@ import {prefix} from './createUniqueIdentification';
 export default function createStoreElement(): HTMLElement {
   const keepAliveDOM = document.createElement('div');
   keepAliveDOM.dataset.type = prefix;
-  keepAliveDOM.style.display = 'none';
+  keepAliveDOM.style.visibility = 'hidden';
+  keepAliveDOM.style.opacity = '0';
+  keepAliveDOM.style.position = 'absolute';
+  keepAliveDOM.style.top = '0';
+  keepAliveDOM.style.left = '0';
+  keepAliveDOM.style.zIndex = '-1';
   document.body.appendChild(keepAliveDOM);
   return keepAliveDOM;
 }
